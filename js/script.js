dffac07b1689da60f242492d1f362fe9f55f93ad
RCRS1234 <rcrsalimbagat@mymail.mapua.edu.ph>
2026-03-06 23:35:30 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
+var images = [
 
+"images/placeholder.jpg",
+
+"images/placeholder.jpg",
+
+"images/placeholder.jpg"
+
+];
+
+var i = 0;
+
+function slideShow(){
+
+document.getElementById("slide").src = images[i];
+
+i++;
+
+if(i >= images.length){
+
+i = 0;
+
+}
+
+setTimeout(slideShow,3000);
+
+}
+
+window.onload = slideShow;
