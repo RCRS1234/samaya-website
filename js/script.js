a54adc869596fe14ba41aec37dbf9fc9baa4945f
RCRS1234 <rcrsalimbagat@mymail.mapua.edu.ph>
2026-03-19 10:34:20 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
-let index = 0;
-showSlides();
 
-function showSlides() {
-  let slides = document.getElementsByClassName("slide");
-
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  index++;
-  if (index > slides.length) {
-    index = 1;
-  }
-
-  slides[index - 1].style.display = "block";
-
-  setTimeout(showSlides, 3000); // change every 3 seconds
-}
 
 function openPopup(img){
 document.getElementById("imagePopup").style.display = "block";
